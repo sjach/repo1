0984be2d3e6826074ea79191070eab78423da1ee
sjach <steve.jachim@gmail.com>
2015-03-21 20:45:07 +0000

    updated

--- main.cpp
+++ main.cpp
@@ -2,11 +2,6 @@
 
 // 21/03/2015  comment to commit
 //changed in github ????? can it do this ????
-<<<<<<< HEAD
-// will it shift this line down?
-=======
-// changed a second time
->>>>>>> 831d14aa4c4ab78094aca7782cfb6a119ce46316
 
 
 xcvxcvxcv
